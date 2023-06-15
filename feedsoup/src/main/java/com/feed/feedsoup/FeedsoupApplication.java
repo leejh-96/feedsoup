a8fe894ef5da66649048917ed64af478c74f7b29
leejh-96 <sbxl12@naver.com>
2023-06-15 14:50:17 +0900

    project initial settings

---
+++ feedsoup/src/main/java/com/feed/feedsoup/FeedsoupApplication.java
@@ -0,0 +1,13 @@
+package com.feed.feedsoup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedsoupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedsoupApplication.class, args);
+	}
+
+}
