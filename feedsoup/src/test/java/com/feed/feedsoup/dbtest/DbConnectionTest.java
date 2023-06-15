1abfcd30c7a135bbb6c5d10e66267700a339b24c
leejh-96 <sbxl12@naver.com>
2023-06-15 15:01:22 +0900

    test code - dbconnectionTest

---
+++ feedsoup/src/test/java/com/feed/feedsoup/dbtest/DbConnectionTest.java
@@ -0,0 +1,33 @@
+package com.feed.feedsoup.dbtest;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@SpringBootTest
+@Slf4j
+public class DbConnectionTest {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String name;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Test
+    @DisplayName("dbConnection 테스트")
+    void dbConnectionTest() throws SQLException {
+        Connection connection = DriverManager.getConnection(url,name,password);
+        log.info("connection: {}", connection);
+    }
+
+}
