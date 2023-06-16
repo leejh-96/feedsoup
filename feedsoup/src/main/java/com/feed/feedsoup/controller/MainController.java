f1dad4f2bdad82d561d7b98a9437ffb3dca70a4b
leejh-96 <sbxl12@naver.com>
2023-06-16 15:44:54 +0900

    [미완성] MainController, main.html

---
+++ feedsoup/src/main/java/com/feed/feedsoup/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.feed.feedsoup.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+
+
+
+
+
+        return "main";
+    }
+}
