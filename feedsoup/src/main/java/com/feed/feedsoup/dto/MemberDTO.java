a393561232940c52cec424d45ecac883ca312019
leejh-96 <sbxl12@naver.com>
2023-06-20 23:57:51 +0900

    [dto-MemberDTO] 수정한거없음

---
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/MemberDTO.java
@@ -0,0 +1,29 @@
+package com.feed.feedsoup.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+public class MemberDTO {
+
+    private int memberNo;
+    private String memberType;
+    private String memberStatus;
+    private String memberId;
+    private String memberPassword;
+    private String memberName;
+    private Date memberBirthdate;
+    private String memberNickname;
+    private String memberPhone;
+    private String memberAddress;
+    private Date memberDate;
+    private Date memberUpdate;
+}
