ee11ddcdca9124bf888946151b0c7e4b13aa1ea7
leejh-96 <sbxl12@naver.com>
2023-06-28 20:00:10 +0900

    [controller-RegisterController] registerSave method save로 method명 변경 세션 검증, 세션객체와 바인딩객체 검증 체크 로직 추가, 폼 바인딩 객체 유효성 검증, 닉네임 중복검사 로직 추가, 회원가입 서비스 호출 로직, 기존 세션과 새로운 세션 생성 로직 추가 session 바인딩 객체 매개변수 추가, httpsession 매개변수 추가, redirect flash 사용하여 일회성으로 session에 저장 로직 추가

--- feedsoup/src/main/java/com/feed/feedsoup/controller/RegisterController.java
+++ feedsoup/src/main/java/com/feed/feedsoup/controller/RegisterController.java
@@ -1,6 +1,8 @@
 package com.feed.feedsoup.controller;
 
+import com.feed.feedsoup.dto.EmailConfirmDTO;
 import com.feed.feedsoup.dto.RegisterFormDTO;
+import com.feed.feedsoup.service.EmailConfirmService;
 import com.feed.feedsoup.service.RegisterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,11 +10,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -20,32 +22,50 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/register")
 public class RegisterController {
 
-    private final RegisterService service;
+    private final RegisterService registerService;
+
+    private final EmailConfirmService emailConfirmService;
 
     @GetMapping("/form")
-    public String register(){
+    public String register(Model model){
+//      회원가입 폼에서 다시 회원가입 폼으로 요청을 했을때 세션의 값을 초기화하기 위함.
+//      httpSession.invalidate();
+        model.addAttribute("registerFormDTO",new RegisterFormDTO());
+
         return "register/registerForm";
     }
 
     @PostMapping("/save")
-    public String registerSave(@Validated @ModelAttribute RegisterFormDTO register,
-                               BindingResult bindingResult,
-                               RedirectAttributes redirect){
-        //RegisterFormDTO 유효성검증
+    public String save(@SessionAttribute(value = "emailConfirm", required = false) EmailConfirmDTO sessionEmailConfirmDTO,
+                       @Validated @ModelAttribute RegisterFormDTO registerFormDTO, BindingResult bindingResult,
+                       HttpSession httpSession,RedirectAttributes redirectAttributes){
+//      세션 검증
+        if (sessionEmailConfirmDTO == null){
+            bindingResult.reject("requiredSession");
+            return "register/registerForm";
+        }
+//      세션객체와 바인딩객체의 이메일,인증번호가 일치하는지 체크
+        if (!emailConfirmService.findByEmailAndValidNum(sessionEmailConfirmDTO,registerFormDTO)){
+            bindingResult.reject("mismatch");
+            return "register/registerForm";
+        }
+////      폼 바인딩 객체의 유효성 검증
         if (bindingResult.hasErrors()){
             return "register/registerForm";
         }
+////      닉네임 중복검사
+        if (!registerService.duplicateNickname(registerFormDTO.getMemberNickname())){
+            bindingResult.reject("duplicateNickname");
+            return "register/registerForm";
+        }
+//
+////      회원가입 서비스 호출하기 && 기존 세션 삭제 후 새로운 세션 생성하기
+        registerService.save(registerFormDTO);
+        httpSession.invalidate();
+//        httpSession.setAttribute("loginMember",registerFormDTO);
+        redirectAttributes.addFlashAttribute("status",true);
 
-        log.info("register : {}",register);
-
-        service.registerSave(register);
-        redirect.addFlashAttribute("status","true");
-
-        //페이지 만들어지면 경로수정하기
         return "redirect:/register/form";
     }
-
-
-
 
 }
