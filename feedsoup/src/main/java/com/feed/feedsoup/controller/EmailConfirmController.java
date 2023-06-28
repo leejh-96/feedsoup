9ed2e9ebf0dcfe89dc56213fe24c74f4b5da62be
leejh-96 <sbxl12@naver.com>
2023-06-28 19:56:39 +0900

    [controller-EmailConfirmController] sendMail method return type 변경, 이메일 유효성 검사, 이메일 중복 체크, 세션 생성, 세션 검증 로직 추가 validNum method return type 변경, 매개변수, session 객체바인딩 매개변수 추가, httpsession 매개변수 추가

--- feedsoup/src/main/java/com/feed/feedsoup/controller/EmailConfirmController.java
+++ feedsoup/src/main/java/com/feed/feedsoup/controller/EmailConfirmController.java
@@ -3,27 +3,78 @@ package com.feed.feedsoup.controller;
 import com.feed.feedsoup.dto.EmailConfirmDTO;
 import com.feed.feedsoup.service.EmailConfirmService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class EmailConfirmController {
 
     private final EmailConfirmService emailService;
+
     @ResponseBody
     @PostMapping("/sendMail")
-    public boolean sendMail(@ModelAttribute EmailConfirmDTO emailConfirmDTO){
+    public Map<String,Object> sendMail(@Validated @ModelAttribute EmailConfirmDTO emailConfirmDTO,
+                                       BindingResult bindingResult,HttpSession httpSession){
 
-        return emailService.findByValidKey(emailConfirmDTO);
+        Map<String,Object> status = new HashMap<>();
+//      이메일 유효성 검사
+        if (bindingResult.hasErrors()){
+            status.put("result", "bindingResult");
+            log.info("bindingResult");
+            return status;
+        }
+//      이메일 중복 체크
+        if (!emailService.duplicateEmail(emailConfirmDTO.getMemberId())){
+            status.put("result", "duplicateEmail");
+            log.info("duplicateEmail");
+            return status;
+        }
+
+        EmailConfirmDTO sessionEmailConfirm = (EmailConfirmDTO)httpSession.getAttribute("emailConfirm");
+        if (sessionEmailConfirm == null){
+            emailService.createSession(emailService.sendMail(emailConfirmDTO),httpSession);
+            status.put("result", "createSessionEmailConfirm");
+            log.info("createSessionEmailConfirm");
+            return status;
+        }
+
+        status.put("result","sessionTrue");
+        log.info("sessionTrue");
+        return status;
     }
+
     @ResponseBody
     @PostMapping("/validNum")
-    public boolean validNum(@ModelAttribute EmailConfirmDTO emailConfirmDTO){
-        return emailService.findByValidNum(emailConfirmDTO);
+    public Map<String,Object> validNum(@SessionAttribute(value = "emailConfirm", required = false) EmailConfirmDTO sessionEmailConfirmDTO,
+                                       @ModelAttribute EmailConfirmDTO emailConfirmDTO,
+                                       HttpSession httpSession){
+
+        Map<String,Object> status = new HashMap<>();
+
+        if (sessionEmailConfirmDTO == null){
+            status.put("result", "sessionEmailConfirmDTONull");
+            return status;
+        }
+
+        if (emailService.findByEmailAndValidNum(sessionEmailConfirmDTO, emailConfirmDTO, httpSession)){
+            status.put("result","validTrue");
+        }else {
+            status.put("result","validFalse");
+        }
+
+        return status;
     }
 
 }
