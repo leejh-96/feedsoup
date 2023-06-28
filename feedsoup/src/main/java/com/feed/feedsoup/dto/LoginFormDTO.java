18c2fa6c24def5e2beee3a3f6548c284cc48bc52
leejh-96 <sbxl12@naver.com>
2023-06-28 20:28:52 +0900

    [dto-LoginFormDTO] loginFormDTO 추가 -login form 데이터만 검증하기 위한 정규식패턴,null,빈값,공백체크 적용

---
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/LoginFormDTO.java
@@ -0,0 +1,20 @@
+package com.feed.feedsoup.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+@Getter@Setter
+@ToString
+public class LoginFormDTO {
+
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    @NotBlank
+    private String memberId;
+
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$")
+    @NotBlank
+    private String memberPassword;
+}
