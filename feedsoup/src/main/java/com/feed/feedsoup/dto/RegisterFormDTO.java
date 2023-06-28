b74a80cf534359b17f72c13f68231c6fc0a6783f
leejh-96 <sbxl12@naver.com>
2023-06-28 20:03:43 +0900

    [dto-RegisterFormDTO] String 타입의 @Size를 @Length로 변경

--- feedsoup/src/main/java/com/feed/feedsoup/dto/RegisterFormDTO.java
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/RegisterFormDTO.java
@@ -2,43 +2,46 @@ package com.feed.feedsoup.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Getter @Setter
+@ToString
 public class RegisterFormDTO {
 
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     @NotBlank
     private String memberId;
 
+    @NotBlank
+    private String checkNum;
+
     @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$")
     @NotBlank
     private String memberPassword;
 
-    @Pattern(regexp = "^[가-힣]{1,20}$")
+    @Pattern(regexp = "^[가-힣]{2,20}$")
     @NotBlank
     private String memberName;
 
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
+//    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotBlank
+    @NotNull
     private Date memberBirthdate;
 
-    @Size(min = 3, max = 20)
+    @Length(min = 3, max = 20)
     @NotBlank
     private String memberNickname;
 
-    @Pattern(regexp = "^\\\\d{3}-\\\\d{3,4}-\\\\d{4}$")
+    @Pattern(regexp = "^\\d{3}-\\d{4}-\\d{4}$")
     @NotBlank
     private String memberPhone;
 
-    @Size(min = 3, max = 100)
+    @Length(min = 3, max = 100)
     @NotEmpty
     private String memberAddress;
 
