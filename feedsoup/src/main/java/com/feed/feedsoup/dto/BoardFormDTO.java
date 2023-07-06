6d913a720eeddb866d2f0008591f471ed41e03e0
leejh-96 <sbxl12@naver.com>
2023-07-06 21:32:46 +0900

    [dto-BoardFormDTO] -validation annotation 적용

--- feedsoup/src/main/java/com/feed/feedsoup/dto/BoardFormDTO.java
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/BoardFormDTO.java
@@ -1,17 +1,27 @@
 package com.feed.feedsoup.dto;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Getter@Setter
 @ToString
 public class BoardFormDTO {
-//   private int memberNo;
+
    private int boardCategoryNo;
    private int boardOptionNo;
+
+   @Length(min = 10, max = 100)
+   @NotEmpty
    private String boardTitle;
+
+   @Length(min = 10, max = 1000)
+   @NotEmpty
    private String boardContent;
+
    private List<MultipartFile> files;
 }
