a31db567a8a675ab03c97223b3accf4670344457
leejh-96 <sbxl12@naver.com>
2023-07-06 21:35:46 +0900

    [dto-BoardCategoryDTO] BoardCategoryDTO 생성

---
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/BoardCategoryDTO.java
@@ -0,0 +1,12 @@
+package com.feed.feedsoup.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter
+public class BoardCategoryDTO {
+
+    private int boardCategoryNo;
+    private String boardCategoryName;
+}
