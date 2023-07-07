e837451b607588ce1ad743b1b4f130da29288deb
leejh-96 <sbxl12@naver.com>
2023-07-07 17:18:00 +0900

    [dto-BoardFormDTO] memberNo field 추가 boardCategoryNo, boardOptionNo @Positive annotation 추가

--- feedsoup/src/main/java/com/feed/feedsoup/dto/BoardFormDTO.java
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/BoardFormDTO.java
@@ -4,23 +4,27 @@ import lombok.*;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter@Setter
 @ToString
 public class BoardFormDTO {
 
+   private int memberNo;
+
+   @Positive
    private int boardCategoryNo;
+
+   @Positive
    private int boardOptionNo;
 
    @Length(min = 10, max = 100)
-   @NotEmpty
+   @NotBlank
    private String boardTitle;
 
    @Length(min = 10, max = 1000)
-   @NotEmpty
+   @NotBlank
    private String boardContent;
 
    private List<MultipartFile> files;
