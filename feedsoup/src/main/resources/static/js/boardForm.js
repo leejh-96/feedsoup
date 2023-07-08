ca1615be94b7d0119ba22cd7b17634009cc5a188
leejh-96 <sbxl12@naver.com>
2023-07-08 22:33:06 +0900

    [js-BoardForm] function findByBoardCategory 삭제 function optionInitialSetting 추가

--- feedsoup/src/main/resources/static/js/boardForm.js
+++ feedsoup/src/main/resources/static/js/boardForm.js
@@ -1,60 +1,58 @@
+// =============select option js Start=============
 $(document).ready(function(){
-    findByBoardCategory();
+    optionInitialSetting();
 });
+function optionInitialSetting(){
+    $('#board-option-select').append('<option value="0" selected>------------선택------------</option>');
+}
 
 document.getElementById("board-category-select").addEventListener("change",function (){
     let value = $('#board-category-select > option:selected').val();
     valueCheck(value);
 });
-
-function findByBoardCategory(){
+function valueCheck(value){
+    if (value === 0 || value === '0'){
+        optionSettings();
+        return;
+    }
+    findByBoardOption(value);
+}
+function findByBoardOption(value){
     $.ajax({
-        url: '/board/findByBoardCategory',
+        url: '/board/findByBoardOption',
         type: 'get',
+        data: {boardCategoryNo : value},
         success: function (obj){
             JSON.stringify(obj);
-            $('#board-category-select').append('<option value="0" selected>------------선택------------</option>');
-            $('#board-option-select').append('<option value="0" selected>------------선택------------</option>');
-            for (let i = 0; i < obj.length; i++) {
-                $('#board-category-select').append('<option value="' + obj[i].boardCategoryNo + '">' + obj[i].boardCategoryName + '</option>');
+            optionSettings();
+            let check = false;
+
+            for (let i = 0; i < obj.length; i++){
+                if (obj[i].boardOptionNo === undefined || obj[i].boardOptionNo === null || obj[i].boardOptionNo === "" || obj[i].boardOptionNo === ''){
+            //세션이 끊어진 상태를 체크함
+                    alert('다시 로그인이 필요합니다.');
+                    break;
+
+                }else{
+            //세션이 유효하다면 option을 그린다.
+                    $('#board-option-select').append('<option value="' + obj[i].boardOptionNo + '" name="boardOptionNo">' + obj[i].boardOptionType + '</option>');
+                }
+                check = true;
             }
+            if (!check) window.location.href="/loginForm";
+
         },
         error: function (errors){
             alert('잠시 후 다시 시도해주세요.');
         }
     });
 }
-
-function valueCheck(value){
-    if (value === 0){
-        optionSettings();
-        return;
-    }
-    findByBoardOption(value);
-}
-
-function findByBoardOption(value){
-    $.ajax({
-       url: '/board/findByBoardOption',
-       type: 'get',
-       data: {boardCategoryNo : value},
-       success: function (obj){
-           JSON.stringify(obj);
-           optionSettings();
-           for (let i = 0; i < obj.length; i++){
-               $('#board-option-select').append('<option value="' + obj[i].boardOptionNo + '" name="boardOptionNo">' + obj[i].boardOptionType + '</option>');
-           }
-       },
-        error: function (errors){
-           alert('잠시 후 다시 시도해주세요.');
-        }
-    });
-}
-
 function optionSettings(){
     $('#board-option-select').empty();
     $('#board-option-select').append('<option value="0" selected>------------선택------------</option>');
 }
+// =============select option js End=============
+// =============file 추가,삭제 btn js Start=============
 let maxFileAttachment = 10;//첨부파일 최대 갯수
 let fileFieldCount = 1;//초기 첨부파일 갯수
 document.getElementById("option-add-btn").addEventListener("click",function (){
@@ -85,3 +83,5 @@ document.getElementById("option-delete-btn").addEventListener("click",function (
         fileFieldCount--;
     }
 });
+// =============file 추가,삭제 btn js End=============
+
