9e2c4ce94a318e5cef33a1dee597dd19dd01a516
leejh-96 <sbxl12@naver.com>
2023-07-08 22:49:33 +0900

    [service-BoardFormService] filePresenceCheck method 추가 saveBoard method 추가 save method name modify saveBoard save method name modify saveBoardAndFile -Transactional작업단위에 게시글+파일등록 작업과 파일 업로드 작업을 묶어서 IOException,NullPointerException이 발생하면 일련의 작업들이 rollback 되게끔 설정함

--- feedsoup/src/main/java/com/feed/feedsoup/service/BoardFormService.java
+++ feedsoup/src/main/java/com/feed/feedsoup/service/BoardFormService.java
@@ -9,13 +9,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BoardFormService {
+
+    private final FileService fileService;
 
     private final BoardFormRepository boardFormRepository;
     public List<BoardCategoryDTO> findByBoardCategory() {
@@ -26,18 +31,28 @@ public class BoardFormService {
         return boardFormRepository.findByBoardOption(boardCategoryNo);
     }
 
-    @Transactional
-    public void save(BoardFormDTO boardFormDTO, List<FileUploadDTO> fileUploadDTOList) {
-        int boardNo = boardFormRepository.saveBoard(boardFormDTO);
-
-        for (FileUploadDTO fileUploadDTO : fileUploadDTOList){
-            fileUploadDTO.setBoardNo(boardNo);
-        }
-
-        boardFormRepository.saveFile(fileUploadDTOList);
+    public boolean filePresenceCheck(List<MultipartFile> multipartFiles) {
+        return fileService.filePresenceCheck(multipartFiles);
     }
 
-    public void save(BoardFormDTO boardFormDTO){
+    public void saveBoard(BoardFormDTO boardFormDTO){
         boardFormRepository.saveBoard(boardFormDTO);
     }
+
+    @Transactional(rollbackFor = {IOException.class, NullPointerException.class})
+    public void saveBoardAndFile(BoardFormDTO boardFormDTO) throws IOException {
+        // DB에 먼저 저장시킨 후 아무런 문제없이 insert 완료 시 절대경로에 파일 업로드
+        // 게시글 등록
+        boardFormRepository.saveBoard(boardFormDTO);
+        // boardFormRepository saveBoard 설정 (useGeneratedKeys="true" keyProperty="boardNo")으로 insert 작업 후 boardFormDTO boardNo에 매핑된다.
+        log.info("boardNo : {}",boardFormDTO.getBoardNo());
+        log.info("boardFormDTO : {}",boardFormDTO);
+        // 비어있는 파일 list는 걸러내고, 비어있지 않은 파일은 FileUploadDTO객체(번호,원본파일명,변경된 파일명)를 list로 리턴
+        List<FileUploadDTO> fileUploadDTOList = fileService.fileCheckAndNewSettings(boardFormDTO.getBoardNo(),boardFormDTO.getFiles());
+        // 파일 등록
+        boardFormRepository.saveFile(fileUploadDTOList);
+        // 파일 업로드
+        fileService.multipleFiles(boardFormDTO.getFiles());
+    }
+
 }
