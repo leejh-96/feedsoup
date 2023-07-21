0672f4043469588b0b1d44227762c9e8698122bc
leejh-96 <sbxl12@naver.com>
2023-07-21 14:35:19 +0900

    [dto-LoginMemberDTO] LoginMemberDTO 추가

---
+++ feedsoup/src/main/java/com/feed/feedsoup/dto/LoginMemberDTO.java
@@ -0,0 +1,22 @@
+package com.feed.feedsoup.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter
+@ToString
+public class LoginMemberDTO {
+
+    private int memberNo;
+
+    private String memberName;
+
+    public LoginMemberDTO() {
+    }
+
+    public LoginMemberDTO(int memberNo, String memberName) {
+        this.memberNo = memberNo;
+        this.memberName = memberName;
+    }
+}
