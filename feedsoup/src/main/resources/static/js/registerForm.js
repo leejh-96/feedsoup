aeb9f5b4b09bccc2969f8ee72fa5e9c966ab681f
leejh-96 <sbxl12@naver.com>
2023-07-21 14:14:48 +0900

    [변경없음]

--- feedsoup/src/main/resources/static/js/registerForm.js
+++ feedsoup/src/main/resources/static/js/registerForm.js
@@ -3,7 +3,7 @@ document.getElementById("memberId").addEventListener("keyup", function() {
 });
 document.getElementById('checkNum').addEventListener('keyup',function (){
     $('#email-verify-num-span').empty();
-})
+});
 document.getElementById("email-verify-btn").addEventListener("click", function() {
     let memberId = $('#memberId').val();
     $('#email-field-error').empty();
@@ -15,11 +15,8 @@ function sendMail(memberId){
         sendMailToServer(memberId,message);
     }else {
         message = '이메일을 형식에 맞게 작성해주세요.';
-        // alert(message);
         $('#email-verify-span').text(message);
     }
-
-    // $('#email-verify-span').text(message);
 }
 
 let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -50,12 +47,11 @@ function sendMailToServer(memberId,message){
             }else if (result === 'duplicateEmail'){
                 message = '이미 사용중인 이메일입니다.';
             }
-            // alert(message);
             $('#email-verify-span').text(message);
         },
         error: function (error){
             alert('잠시 후 다시 시도해주세요.');
-            console.log('ajax 통신 실패');
+            console.log('ajax 통신 실패'+error);
         }
     });
 }
@@ -83,22 +79,20 @@ function validNum(memberId,checkNum,message){
                 message = '이메일과 인증번호가 올바르지 않습니다. 다시 인증해주세요.';
             }else if (result === 'validTrue'){
                 message = '인증되었습니다.';
-                // $('#email-group > input, #email-group > button').prop('disabled', true);
             }
-            // alert(message);
             $('#email-verify-num-span').text(message);
         },
         error: function (error){
             alert('잠시 후 다시 시도해주세요.');
-            console.log('ajax 통신 실패');
+            console.log('ajax 통신 실패'+error);
         }
-    })
+    });
 }
 
 //주소 api
 document.getElementById('post-code').addEventListener('click',function (){
     postcode();
-})
+});
 function postcode(){
     new daum.Postcode({
         oncomplete: function(data) {
@@ -106,6 +100,8 @@ function postcode(){
         }
     }).open();
 }
+
+
 
 // document.getElementById('memberNickname').addEventListener('change',function (){
 //     let memberNickname = $('#memberNickname').val();
@@ -120,47 +116,47 @@ function postcode(){
 // document.getElementById('submit-btn').addEventListener('click',function (){
 //     submitCheck();
 // })
-function submitCheck(event){
-    event.preventDefault();
-    let memberId = $('#memberId').val();
-    let checkNum = $('#checkNum').val();
-    let message = '';
-    $.ajax({
-        url: '/submitCheck',
-        method: 'post',
-        data: {memberId : memberId,checkNum : checkNum},
-        async: true,
-        success: function (status){
-            let result = status['result'];
-            if (result === 'requiredMailCheck'){
-                message = '메일 인증이 필요한 서비스입니다.';
-                $('#email-verify-span').text(message);
-                return false;
-            }else if (result === null){
-                message = '유효시간이 경과되어 다시 메일 인증이 필요합니다.';
-                $('#email-verify-span').text(message);
-                return false;
-            }else if (result === 'N'){
-                message = '인증번호를 인증해주세요.';
-                $('#email-verify-num-span').text(message);
-                return false;
-            }else if (result === 'Y'){
-                if (result === false){
-                    message = '인증번호가 일치하지 않습니다.';
-                    $('#email-verify-num-span').text(message);
-                    return false;
-                }else {
-                    console.log('submit가즈아');
-                    return true;
-                }
-            }
-        },
-        error: function (error){
-            console.log('submitCheck ajax error');
-            return false;
-        }
-    })
-}
+// function submitCheck(event){
+//     event.preventDefault();
+//     let memberId = $('#memberId').val();
+//     let checkNum = $('#checkNum').val();
+//     let message = '';
+//     $.ajax({
+//         url: '/submitCheck',
+//         method: 'post',
+//         data: {memberId : memberId,checkNum : checkNum},
+//         async: true,
+//         success: function (status){
+//             let result = status['result'];
+//             if (result === 'requiredMailCheck'){
+//                 message = '메일 인증이 필요한 서비스입니다.';
+//                 $('#email-verify-span').text(message);
+//                 return false;
+//             }else if (result === null){
+//                 message = '유효시간이 경과되어 다시 메일 인증이 필요합니다.';
+//                 $('#email-verify-span').text(message);
+//                 return false;
+//             }else if (result === 'N'){
+//                 message = '인증번호를 인증해주세요.';
+//                 $('#email-verify-num-span').text(message);
+//                 return false;
+//             }else if (result === 'Y'){
+//                 if (result === false){
+//                     message = '인증번호가 일치하지 않습니다.';
+//                     $('#email-verify-num-span').text(message);
+//                     return false;
+//                 }else {
+//                     console.log('submit가즈아');
+//                     return true;
+//                 }
+//             }
+//         },
+//         error: function (error){
+//             console.log('submitCheck ajax error');
+//             return false;
+//         }
+//     })
+// }
 
 
 // let count = 0;
