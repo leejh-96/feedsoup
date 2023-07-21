99a1b1aa79509655bb14c9b76066704cb39963a6
leejh-96 <sbxl12@naver.com>
2023-07-21 13:58:27 +0900

    [controller-BoardFormController] saveForm method 수정

--- feedsoup/src/main/java/com/feed/feedsoup/controller/BoardFormController.java
+++ feedsoup/src/main/java/com/feed/feedsoup/controller/BoardFormController.java
@@ -4,15 +4,21 @@ import com.feed.feedsoup.dto.*;
 import com.feed.feedsoup.service.BoardFormService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import javax.swing.plaf.PanelUI;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -25,10 +31,10 @@ public class BoardFormController {
     private final BoardFormService boardFormService;
 
     @GetMapping("/form")
-    public String form(@SessionAttribute(value = "loginMember") MemberDTO memberDTO,
+    public String form(@SessionAttribute(value = "loginMember") LoginMemberDTO loginMemberDTO,
                        HttpSession httpSession, Model model){
-        log.info("loginMember : {}",memberDTO);
-        httpSession.setAttribute("memberNo",memberDTO.getMemberNo());
+        log.info("loginMember : {}",loginMemberDTO);
+        httpSession.setAttribute("memberNo",loginMemberDTO.getMemberNo());
         model.addAttribute("boardFormDTO",new BoardFormDTO());
         model.addAttribute("boardCategorylist",boardFormService.findByBoardCategory());
         return "board/boardForm";
@@ -42,27 +48,22 @@ public class BoardFormController {
 
     @PostMapping("/form")
     public String saveForm(@Validated @ModelAttribute BoardFormDTO boardFormDTO,BindingResult bindingResult,
-                           @SessionAttribute(value = "memberNo") int memberNo,
+                           @SessionAttribute(value = "loginMember") LoginMemberDTO loginMemberDTO,Model model,
                            RedirectAttributes redirectAttributes) throws IOException {
 
+        model.addAttribute("boardCategorylist",boardFormService.findByBoardCategory());
         if (bindingResult.hasErrors()){
             return "board/boardForm";
         }
         // 작성자의 식별번호를 세션에서 얻어와서 boardFormDTO에 세팅
-        boardFormDTO.setMemberNo(memberNo);
-        //  filePresenceCheck는 다중 파일의 경우 list 순회를 통해 파일의 존재 여부를 체크함
-        //  존재한다면 false, 존재하지 않는다면 true 리턴
-        if (boardFormService.filePresenceCheck(boardFormDTO.getFiles())){
-            //파일이 비어있다면 게시글만 insert
-            boardFormService.saveBoard(boardFormDTO);
-        }else {
-            //파일이 비어있지 않다면 게시글 + 파일 insert
-            boardFormService.saveBoardAndFile(boardFormDTO);
-        }
+        boardFormDTO.setMemberNo(loginMemberDTO.getMemberNo());
+
+        boardFormService.saveBoard(boardFormDTO);
+
 //      향후에 바로 상세페이지로 보여주거나 list에서 게시된 글 표시할때 쓰기
         redirectAttributes.addFlashAttribute("status",true);
 //      나중에 상세페이지로 바뀌게되면 url 바꾸기
-        return "redirect:/board/list";
+        return "redirect:/board";
     }
 
 }
