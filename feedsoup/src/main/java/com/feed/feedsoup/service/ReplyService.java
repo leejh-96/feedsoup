37383ff1c846fbcd76532513d036fe73325a22a0
leejh-96 <sbxl12@naver.com>
2023-07-21 14:27:36 +0900

    [service-ReplyService] ReplyService 추가

---
+++ feedsoup/src/main/java/com/feed/feedsoup/service/ReplyService.java
@@ -0,0 +1,23 @@
+package com.feed.feedsoup.service;
+
+import com.feed.feedsoup.repository.ReplyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ReplyService {
+
+    private final ReplyRepository replyRepository;
+
+
+    public void saveReply(int boardNo, String reply, int memberNo) {
+        replyRepository.saveReply(boardNo,reply,memberNo);
+    }
+
+    public boolean deleteReply(int boardNo, int replyNo) {
+        return replyRepository.deleteReply(boardNo, replyNo) > 0 ? true : false;
+    }
+}
