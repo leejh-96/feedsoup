e522de95bacea383b58fab9a620e7934436f6b53
leejh-96 <sbxl12@naver.com>
2023-07-21 14:02:01 +0900

    [util-PageInfo] -생성자 추가

--- feedsoup/src/main/java/com/feed/feedsoup/util/PageInfo.java
+++ feedsoup/src/main/java/com/feed/feedsoup/util/PageInfo.java
@@ -1,47 +1,39 @@
 package com.feed.feedsoup.util;
 
+import com.feed.feedsoup.dto.SearchDTO;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageInfo {
 
     private int currentPage;//현재페이지
     private int totalNumberPosts;//총게시글수
-    private final int PAGE_POSTS_LIMIT = 10;//한 페이지 게시글 수
-    private final int PAGE_LIST_LIMIT = 10;//한 페이지에 표시될 리스트 수
+    private int pagePostsLimit;//한 페이지 게시글 수
+    private int pageListLimit;//한 페이지에 표시될 리스트 수
+
+    public PageInfo(int currentPage, int totalNumberPosts) {
+        this.currentPage = currentPage;
+        this.totalNumberPosts = totalNumberPosts;
+        this.pagePostsLimit = 10;
+        this.pageListLimit = 10;
+    }
 
     //전체 페이지 중 가장 마지막 페이지->101 / 10 = 10.1 => 11페이지
     public int getMaxPage(){
-        return (int)Math.ceil((double)this.totalNumberPosts / this.currentPage);
+        return (int)Math.ceil((double)this.totalNumberPosts / this.pageListLimit);
     }
 
     //페이징 된 페이지 중 시작 페이지 -> 1... 11... 21... 31...
     public int getStartPage(){
-        return (this.PAGE_POSTS_LIMIT * ((this.currentPage - 1) / this.PAGE_POSTS_LIMIT)) + 1;
+        return (this.pagePostsLimit * ((this.currentPage - 1) / this.pagePostsLimit)) + 1;
     }
 
     //페이징 된 페이지 중 마지막 페이지->10, 20, 30, 40, ....
     public int getEndPage(){
-        int endPage = this.getStartPage() + this.PAGE_POSTS_LIMIT - 1;
+        int endPage = this.getStartPage() + this.pagePostsLimit - 1;
         return endPage > this.getMaxPage() ? this.getMaxPage() : endPage;
-    }
-
-    //현재 페이지에서 시작되는 게시글의 번호
-    public int getStartList() {
-        return (this.getCurrentPage() - 1) * this.PAGE_LIST_LIMIT + 1;
-    }
-
-    //현재 페이지의 마지막 게시글의 번호
-    public int getEndList() {
-        int endList = this.getStartList() + this.PAGE_LIST_LIMIT - 1;
-        return endList > this.totalNumberPosts ? this.totalNumberPosts : endList;
-    }
-
-    //현재 페이지
-    public int getCurrentPage() {
-        return this.currentPage;
-    }
-
-    //하나의 페이지에 표시될 페이지 수
-    public int getPageListLimit() {
-        return this.PAGE_LIST_LIMIT;
     }
 
     //이전 페이지
@@ -56,5 +48,29 @@ public class PageInfo {
         return nextPage > this.getMaxPage() ? this.getMaxPage() : nextPage;
     }
 
+    //현재 페이지에서 시작되는 게시글의 번호
+    public int getStartList() {
+        return (this.getCurrentPage() - 1) * this.pageListLimit + 1;
+    }
+
+    //현재 페이지의 마지막 게시글의 번호
+    public int getEndList() {
+        int endList = this.getStartList() + this.pageListLimit - 1;
+        return endList > this.totalNumberPosts ? this.totalNumberPosts : endList;
+    }
+
+    public void pageSettings(){
+        if (this.getCurrentPage() > this.getEndPage()){
+            this.setCurrentPage(1);
+        }
+    }
+
+    public int getLimit(){
+        return this.getPageListLimit();
+    }
+
+    public int getOffset(){
+        return (this.getCurrentPage() - 1) * this.getLimit();
+    }
 
 }
