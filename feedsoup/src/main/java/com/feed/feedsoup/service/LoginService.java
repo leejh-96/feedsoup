2d7a86b5f6c92df81305e7403382ef9a9ba2b9ba
leejh-96 <sbxl12@naver.com>
2023-07-31 13:27:07 +0900

    [변경없음]

--- feedsoup/src/main/java/com/feed/feedsoup/service/LoginService.java
+++ feedsoup/src/main/java/com/feed/feedsoup/service/LoginService.java
@@ -1,18 +1,22 @@
 package com.feed.feedsoup.service;
 
 import com.feed.feedsoup.dto.LoginFormDTO;
+import com.feed.feedsoup.dto.LoginMemberDTO;
 import com.feed.feedsoup.dto.MemberDTO;
 import com.feed.feedsoup.repository.LoginRepository;
+import com.feed.feedsoup.util.PasswordEncryption;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
     private final LoginRepository loginRepository;
 
-    public MemberDTO login(LoginFormDTO loginFormDTO) {
+    public MemberDTO getMemberInfo(LoginFormDTO loginFormDTO) {
         return loginRepository.login(loginFormDTO);
     }
 
